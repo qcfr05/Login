4b7207eca29c16486ced4fe82121cdb3007fd151
qcfr05 <38660561+qcfr05@users.noreply.github.com>
2018-04-23 10:43:09 -0400

    Update Login-Git-Download.bat

--- Login-Git-Download.bat
+++ Login-Git-Download.bat
@@ -1,4 +1,4 @@
 @echo off
 git clone git://github.com/qcfr05/Login.git
 git web--browse https://github.com/qcfr05/Login.git
-cd\ && start %userprofile%\login\Login.html
+cd\ && start %userprofile%\login\Login.html && exit
