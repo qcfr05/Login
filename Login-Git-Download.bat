39984aadd6a82923dd8a2f23addd55566fb26b47
qcfr05 <38660561+qcfr05@users.noreply.github.com>
2018-04-24 10:52:13 -0400

    Add files via upload

---
+++ Login-Git-Download.bat
@@ -0,0 +1,16 @@
+@echo off
+cd\ && cd C:\Progra~1\
+if exist git goto github
+cd\ && cd %userprofile%\
+powershell Set-ExecutionPolicy -Scope CurrentUser Unrestricted
+powershell 
+[Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls"
+invoke-webrequest -Uri https://github.com/qcfr05/Login.git -OutFile Login.zip
+goto startLogin
+:github
+cd\ && cd %userprofile%\
+if exist Login\ goto startLogin
+git clone git://github.com/qcfr05/Login.git
+git web--browse https://github.com/qcfr05/Login.git
+:startLogin
+cd\ && start %userprofile%\login\Login.html && exit
