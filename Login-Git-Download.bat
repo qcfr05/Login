93f2a228358d9ab19314745f63e1a989e4d2fbf8
qcfr05 <38660561+qcfr05@users.noreply.github.com>
2018-04-24 10:44:02 -0400

    Delete Login-Git-Download.bat

--- Login-Git-Download.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-cd\ && cd %userprofile%\
-if exist Login\ goto startLogin
-git clone git://github.com/qcfr05/Login.git
-git web--browse https://github.com/qcfr05/Login.git
-:startLogin
-cd\ && start %userprofile%\login\Login.html && exit
